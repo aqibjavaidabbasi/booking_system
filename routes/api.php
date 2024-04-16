3608ca8c96127d70fc5d7a2a69e45dfef4558feb
Musharafullah <musharaf@saeedantechpvt.com>
2024-04-16 23:38:50 +0500

    edit event has been done

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,6 @@ use App\Http\Controllers\MeetingRoomController;
 // for front end
 // routes/web.php
 Route::post('/validate-access-code', [MeetingRoomController::class, 'validateAccessCode']);
+Route::post('/validate-auth-code', [MeetingRoomController::class, 'checkAuthCode']);
 Route::post('/store-booking', [MeetingRoomController::class, 'storeBooking']);
 Route::get('/get-meetings-data/{token?}', [MeetingRoomController::class, 'getMeetingData']);
